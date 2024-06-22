1d582b55fcf6f6f753de996c94eadea45ff3a6a3
Jacob Hrbek <kreyren@fsfe.org>
2024-06-22 10:41:01 +0000

    security: Enforce systemd-boot editor as false

--- src/nixos/modules/system/security/security.nix
+++ src/nixos/modules/system/security/security.nix
@@ -6,4 +6,6 @@ let
 	inherit (lib) mkForce;
 in {
 	system.copySystemConfiguration = mkForce false; # Do not copy system configuration as it will be incomplete and may contain secrets
+
+	boot.loader.systemd-boot.editor = mkForce false; # Do not allow systemd-boot editor as it's set `true` by default for user convicience and can be used to inject root commands to the system
 }
