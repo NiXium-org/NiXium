2e113bf5b4031fb76715d31db42c915761cbf4e8
Jacob Hrbek <kreyren@fsfe.org>
2024-06-29 18:38:50 +0000

    kreyren: Add hexchat in impermanence

--- src/nixos/users/kreyren/home/modules/system/impermanence/impermanence.nix
+++ src/nixos/users/kreyren/home/modules/system/impermanence/impermanence.nix
@@ -17,6 +17,9 @@ in {
 			".ssh"
 			".cache"
 
+			# FIXME-QA(Krey): This should be applied only when hexchat is installed
+			".config/hexchat"
+
 			# FIXME-QA(Krey): This should be applied only when simplex is installed
 			".local/share/simplex"
 
