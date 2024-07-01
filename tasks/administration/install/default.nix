0605edf8f12881413d5b292b74cdd1e505c6bcf0
Jacob Hrbek <kreyren@fsfe.org>
2024-07-01 09:49:45 +0000

    Try to implement TLATER's solution

--- tasks/administration/install/default.nix
+++ tasks/administration/install/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 
 # The Install Task is used to deploy a new system via disko-install
 
@@ -11,7 +11,7 @@
 				description = "Perform full declarative reinstallation of SYSTEM on a set DISK";
 				category = "Administration";
 				# FIXME-QA(Krey): This makes the declaration more functional, but looks like an ugly hack
-				exec = toString ((import ./script.nix { inherit pkgs; }).install-task + /bin/install-task);
+				exec = toString ((import ./script.nix { inherit pkgs inputs; }).install-task + /bin/install-task);
 			};
 		};
 	};
--- tasks/administration/install/script.nix
+++ tasks/administration/install/script.nix
@@ -1,11 +1,11 @@
-{ pkgs, inputs, ... }:
+{ pkgs, ... }:
 
 # Task to INSTALL the specified derivation on current system including the firmware in a fully declarative way
 
 # Refer to https://github.com/nix-community/disko/issues/657#issuecomment-2146978563 for implementation notes
 
 {
-	perSystem = { system, ... }: {
+	perSystem = { system, inputs, ... }: {
 		install-task = pkgs.writeShellScriptBin "install-task" ''
 			system="$1"
 			disk="$2"
