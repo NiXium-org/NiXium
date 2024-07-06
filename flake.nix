1ea34b7fb732012a48b71d877e5973abd69e3f99
Jacob Hrbek <kreyren@fsfe.org>
2024-07-06 16:17:11 +0000

    codium: use nano as default EDITOR

--- flake.nix
+++ flake.nix
@@ -140,6 +140,7 @@
 						inputs.nixpkgs.legacyPackages.${system}.nil # Needed for linting
 						inputs.nixpkgs.legacyPackages.${system}.nixpkgs-fmt # Nixpkgs formatter
 						inputs.nixpkgs.legacyPackages.${system}.git # Working with the codebase
+						inputs.nixpkgs.legacyPackages.${system}.nano # Editor to work with the codebase in cli
 
 						inputs.ragenix.packages.${system}.default # To manage secrets
 						inputs.nixpkgs.legacyPackages.${system}.age # Managing age files
