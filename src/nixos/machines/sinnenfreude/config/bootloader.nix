c7ad60450a3caba233ac1f378460a0cac148f7d5
Jacob Hrbek <kreyren@fsfe.org>
2024-07-13 04:25:10 +0000

    sync

--- src/nixos/machines/sinnenfreude/config/bootloader.nix
+++ src/nixos/machines/sinnenfreude/config/bootloader.nix
@@ -6,7 +6,5 @@
 	boot.lanzaboote.enable = true; # Whether to use NixOS's implementation of secure-boot
 	boot.loader.systemd-boot.enable = false;
 
-	security.tpm2.enable = true;
-
 	boot.loader.efi.canTouchEfiVariables = true;
 }
