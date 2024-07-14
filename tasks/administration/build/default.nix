ec5a7e8b4f113de4cbd295221d6265e52454d629
Jacob Hrbek <kreyren@fsfe.org>
2024-07-14 15:11:16 +0000

    Tag problem with build task

--- tasks/administration/build/default.nix
+++ tasks/administration/build/default.nix
@@ -9,6 +9,7 @@
 				description = "Build and cache the system configuration without deployment";
 				category = "Administration";
 				# FIXME-QA(Krey): This makes the declaration more functional, but looks like an ugly hack
+				# FIXME(Krey): Does not accept arguments, likely a mission-control bug as the declaration is same with deploy where it works without issues
 				exec = toString ((import ./script.nix { inherit pkgs; }).build-task + /bin/build-task);
 			};
 		};
