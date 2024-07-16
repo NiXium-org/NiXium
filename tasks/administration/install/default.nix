1d6f5af6096c7259ea48a899fd323091af75910b
Jacob Hrbek <kreyren@fsfe.org>
2024-07-16 04:20:07 +0000

    sync

--- tasks/administration/install/default.nix
+++ tasks/administration/install/default.nix
@@ -1,28 +1,35 @@
-{ ... }:
+{ inputs, self, ... }:
 
-# The Install Task
+# Task to INSTALL the specified derivation on current system including the firmware in a fully declarative way
+
+# WARNING: This task will cause a data loss unless the configuration is 100% declarative including the persistent files
+
+# Refer to https://github.com/nix-community/disko/issues/657#issuecomment-2146978563 for implementation notes
 
 {
-	perSystem = { pkgs, ... }: {
+	perSystem = { system, pkgs, inputs', self', ... }: {
+		# Integration in mission-control
 		mission-control.scripts = {
 			"install" = {
-				description = "Perform declaration installation on the specified distribution, system and release";
+				description = "Perform full declarative reinstallation of SYSTEM on a set DISK";
 				category = "Administration";
+				exec = ''
+					distribution="$1"
+					system="$2"
 
-				exec = pkgs.writeShellApplication {
-					name = "tasks-install";
+					die() { printf "FATAL: %s/n" "$2"; exit ;}
 
-					runtimeInputs = [
-						pkgs.nixos-install-tools
-						pkgs.nixos-rebuild
-						pkgs.gnused
-						pkgs.git
-					];
+					# Input check
+					[ -n "$distribution" ] || die 1 "First Argument (distribution) is required for the install task"
+					[ -n "$system" ] || die 1 "Second Argument (system) is required for the install task"
 
-					# FIXME(Krey): This should use flake-root to set absolute path
-					text = builtins.readFile ./tasks-install.sh;
-				};
+					echo "WARNING: This action will wipe all data on the target device and performs full declarative re-installation!"
+
+					# Perform the task
+					nix --extra-experimental-features 'nix-command flakes' run ".#packages.x86_64-linux.$distribution-$system-install"
+				'';
 			};
 		};
 	};
 }
+
