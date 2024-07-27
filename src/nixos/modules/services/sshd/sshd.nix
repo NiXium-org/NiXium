2effae7210b84940a09b6c7eb54e530e94707020
Jacob Hrbek <kreyren@fsfe.org>
2024-07-27 17:38:08 +0000

    ssh: Adjust to not allow keyboard interactive login

--- src/nixos/modules/services/sshd/sshd.nix
+++ src/nixos/modules/services/sshd/sshd.nix
@@ -2,14 +2,17 @@
 
 # Global configuration of SSHD
 
+# We are using pubkeys for the whole infra as they can't be bruteforced and are more functional
+
 let
 	inherit (lib) mkIf mkForce mkDefault;
 in mkIf config.services.openssh.enable {
-	# We are using pubkeys for the whole infra as they can't be bruteforced and are more functional
 	services.openssh.settings.PasswordAuthentication = mkForce false; # Password Authentification prohibited, require pubkeys
 
 	services.openssh.openFirewall = mkDefault false; # Do not automatically open the firewall
 
-	services.openssh.settings.KexAlgorithms = mkForce [ "sntrup761x25519-sha512@openssh.com" ]; # Allow only Post-Quantum Kex Algorithms
+	services.openssh.settings.KexAlgorithms = mkForce [ "sntrup761x25519-sha512@openssh.com" ]; # Allow only Post-Quantum Key Exchange Algorithms
+
+	services.openssh.settings.KbdInteractiveAuthentication = mkForce false; # Do not allow keyboard interactive authorization
 }
 
