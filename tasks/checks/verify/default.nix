5f609cd96c7994490c3cd650baa846526cbf414e
Jacob Hrbek <kreyren@fsfe.org>
2024-07-27 17:38:30 +0000

    tasks/verify: Adjustments for  all task

--- tasks/checks/verify/default.nix
+++ tasks/checks/verify/default.nix
@@ -1,26 +1,47 @@
 { ... }:
 
-# POLICY: non-fatal failures on NixOS with releases OTHER-THAN stable are to be ignored, stable always actionable.
-
 {
-	perSystem = { inputs', pkgs, ... }: {
+	perSystem = { inputs', ... }: {
 		mission-control.scripts = {
-			verify = { # No argument || [DISTRO] [SYSTEM] (RELEASE)
-				description = "Verify the system declaration per distribution and release or current if no argument is provided";
+			verify = {
+				description = "Verify the declaration of all systems or specified";
 				category = "Checks";
+				exec = ''
+					case "$*" in
+						"all") # Verify All Systems
+							for system in $(find "$FLAKE_ROOT/src/nixos/machines/"* -maxdepth 0 -type d | sed "s#^$FLAKE_ROOT/src/nixos/machines/##g" | tr '\n' ' '); do
+								status=$(cat "$FLAKE_ROOT/src/nixos/machines/$system/status")
+								case "$status" in
+									"OK")
+										echo "Checking system: $system"
 
-				exec = pkgs.writeShellApplication {
-					name = "tasks-verify";
-
-					runtimeInputs = [
-						pkgs.git
-						pkgs.gnused
-						pkgs.nixos-rebuild
-						pkgs.nixos-install-tools
-					];
-
-					text = builtins.readFile ./tasks-verify.sh;
-				};
+										${inputs'.nixpkgs.legacyPackages.nixos-rebuild}/bin/nixos-rebuild \
+											dry-build \
+											--flake "git+file://$FLAKE_ROOT#$system" \
+											--option eval-cache false \
+											--show-trace || echo "WARNING: System '$system' failed evaluation!"
+									;;
+									"WIP") echo "Configuration for system '$system' is marked a Work-in-Progress, skipping build.." ;;
+									*) echo "System '$system' reports undeclared status state: $status"
+								esac
+							done
+						;;
+						"") # Verify Current System
+							hostname="$(hostname --short)"
+							echo "Checking system: $hostname"
+							${inputs'.nixpkgs.legacyPackages.nixos-rebuild}/bin/nixos-rebuild dry-build \
+								--flake "git+file://$FLAKE_ROOT#$hostname" \
+								--option eval-cache false \
+								--show-trace
+						;;
+						*) # Verify System By (derivation) Name
+							echo "Checking system: $*"
+							${inputs'.nixpkgs.legacyPackages.nixos-rebuild}/bin/nixos-rebuild dry-build \
+							--flake "git+file://$FLAKE_ROOT#$*" \
+							--option eval-cache false \
+							--show-trace
+					esac
+				'';
 			};
 		};
 	};
