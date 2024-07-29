d0974d7c60af55e9d1590943933ea4bf3a6c054d
Jacob Hrbek <kreyren@fsfe.org>
2024-07-29 00:56:40 +0000

    sync

--- src/nixos/machines/sinnenfreude/config/hardware-acceleration.nix
+++ src/nixos/machines/sinnenfreude/config/hardware-acceleration.nix
@@ -7,6 +7,7 @@
 let
 	inherit (lib) mkMerge mkIf;
 in {
+	# FIXME-QA(Krey): I hate this solution, this would work much better as case statement, but that causes infinite recursion..
 	config = mkMerge [
 		(mkIf (config.system.nixos.release == "24.05") {
 			hardware.opengl = {
@@ -16,12 +17,9 @@ in {
 			};
 		})
 
-		# Everything else
-		(mkIf (config.system.nixos.release != "24.05") {
-			hardware.opengl = {
-				enable = true;
-				driSupport32Bit = true;
-			};
+		(mkIf (config.system.nixos.release == "24.11") {
+			hardware.graphics.enable = true;
+			hardware.graphics.enable32Bit = true;
 		})
 	];
 }
--- src/nixos/machines/tupac/config/hardware-acceleration.nix
+++ src/nixos/machines/tupac/config/hardware-acceleration.nix
@@ -1,12 +1,23 @@
-{ ... }:
+{ config, lib, ... }:
 
 # Hardware-acceleration management of TUPAC
 
-{
-	# Hardware acceleration
-	hardware.opengl = {
-		enable = true;
-		driSupport = true;
-		driSupport32Bit = true;
-	};
+let
+	inherit (lib) mkMerge mkIf;
+in {
+	# FIXME-QA(Krey): I hate this solution, this would work much better as case statement, but that causes infinite recursion..
+	config = mkMerge [
+		(mkIf (config.system.nixos.release == "24.05") {
+			hardware.opengl = {
+				enable = true;
+				driSupport = true;
+				driSupport32Bit = true;
+			};
+		})
+
+		(mkIf (config.system.nixos.release == "24.11") {
+			hardware.graphics.enable = true;
+			hardware.graphics.enable32Bit = true;
+		})
+	];
 }
