b0f358fbe32065a804fcc95a167a9752d7f947b8
Jacob Hrbek <kreyren@fsfe.org>
2024-08-13 20:34:56 +0200

    sync

--- src/nixos/images/default.nix
+++ src/nixos/images/default.nix
@@ -1,3 +1,3 @@
 {
-	imports = [./test ];
+	imports = [./minimal ];
 }
--- src/nixos/machines/tsvetan/releases/stable/tsvetan-nixos-stable-install.sh
+++ src/nixos/machines/tsvetan/releases/stable/tsvetan-nixos-stable-install.sh
@@ -20,11 +20,11 @@ ragenixIdentity="$HOME/.ssh/id_ed25519"
 # Manage Ragenix - The Rust implementation of age for managing secret files
 	# If the directory is not a symlink already then assume that the deployment is requested on a non-NiXium OS and mimic the file hierarchy
 	[ -L "$ragenixTempDir" ] || {
-		[ -d "$ragenixTempDir.d/1" ] || sudo mkdir "$ragenixTempDir.d/1"
-		ln -s "$ragenixTempDir" "$ragenixTempDir.d/1"
+		[ -d "$ragenixTempDir.d/1" ] || sudo mkdir --parents "$ragenixTempDir.d/1"
+		ln -s "$ragenixTempDir.d/1" "$ragenixTempDir"
 
 		# Ensure correct permission
-		sudo chown -R "root:keys" "$ragenixTempDir"
+		sudo chown -R root:keys "$ragenixTempDir"
 		sudo chmod -R 400 "$ragenixTempDir"
 	}
 
@@ -37,7 +37,7 @@ ragenixIdentity="$HOME/.ssh/id_ed25519"
 	swapFile="/swapfile"
 	# shellcheck disable=SC2312 # Upstream bug, should not output with the `|| echo 0` used as the de-factor '|| true' recommended
 	[ "$(awk '/SwapTotal/ {print $2}' /proc/meminfo || echo 0)" -ge 4194304 ] || {
-		swapon -s | grep -q "$swapFile" || esudo swapoff "$swapFile" # Deactivate the swapfile if it's already there so that we can resize it
+		! swapon -s | grep -q "$swapFile" || esudo swapoff "$swapFile" # Deactivate the swapfile if it's already there so that we can resize it
 		[ "$(stat -c%s "$swapFile" || echo 0)" -lt 4194304 ] || esudo dd if=/dev/zero of="$swapFile" bs=1M count=4096 conv=notrunc # Resize the swap file to the desired size
 
 		# Ensure correct permissions
