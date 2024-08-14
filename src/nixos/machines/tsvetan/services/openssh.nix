3d255e4076c26bfd6650b859267cdf07ccc5ecdb
Jacob Hrbek <kreyren@fsfe.org>
2024-08-14 14:23:01 +0200

    sync

--- src/nixos/machines/tsvetan/services/openssh.nix
+++ src/nixos/machines/tsvetan/services/openssh.nix
@@ -6,21 +6,21 @@ let
 	inherit (lib) mkIf mkForce;
 in mkIf config.services.openssh.enable {
 	# Import the private key for an onion service
-	# age.secrets.tsvetan-onion-openssh-private = {
-	# 	file = ../secrets/tsvetan-onion-openssh-private.age;
+	age.secrets.tsvetan-onion-openssh-private = {
+		file = ../secrets/tsvetan-onion-openssh-private.age;
 
-	# 	owner = "tor";
-	# 	group = "tor";
+		owner = "tor";
+		group = "tor";
 
-	# 	path = "/var/lib/tor/onion/openssh/hs_ed25519_secret_key";
+		path = "/var/lib/tor/onion/openssh/hs_ed25519_secret_key";
 
-	# 	symlink = false; # Appears to not work as symlink
-	# };
+		symlink = false; # Appears to not work as symlink
+	};
 
 	services.tor.relay.onionServices."openssh".map = mkIf config.services.tor.enable config.services.openssh.ports; # Provide hidden SSH
 
 	# Set the pubkey
-	# environment.etc."ssh/ssh_host_ed25519_key.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP8d9Nz64gE+x/+Dar4zknmXMAZXUAxhF1IgrA9DO4Ma root@mracek";
+	environment.etc."ssh/ssh_host_ed25519_key.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7kiI7Y5lqjdcPIbrJkStyM0peCiUZ0VDNzEYV/0Ycr root@mracek";
 
 	services.openssh.hostKeys = mkForce []; # Do not generate SSH keys
 
