a8d0d021e7c4b2a3ba496c9569f6727d98fdd44f
Jacob Hrbek <kreyren@fsfe.org>
2024-08-14 23:29:45 +0200

    use hardened 6.9 kernel

--- src/nixos/machines/tsvetan/config/kernel.nix
+++ src/nixos/machines/tsvetan/config/kernel.nix
@@ -1,8 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 # Kernel Management of TSVETAN
 
 {
+	boot.kernelPackages = pkgs.linuxPackages_6_9_hardened; # Use later version of the hardened kernel as it contains patches to broken kernel modules
+
 	boot.kernelParams = [
 		"console=ttyS0,115200n8"
 		"console=tty0"
