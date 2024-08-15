e4eba170cb69cab9dff0f07eec4b6ccf9e58fbe2
Jacob Hrbek <kreyren@fsfe.org>
2024-08-15 14:25:37 +0200

    sync

--- src/nixos/machines/tsvetan/releases/master/tsvetan-nixos-master-hijack.sh
+++ src/nixos/machines/tsvetan/releases/master/tsvetan-nixos-master-hijack.sh
@@ -6,16 +6,15 @@ echo "$systemDeviceBlock" >/dev/null
 echo "$secretTsvetanPasswordPath" >/dev/null
 echo "$secretTsvetanKeyPath" >/dev/null
 
+targetSystem="root@192.168.0.62"
+
 # FIXME-QA(Krey): This should be a runtimeInput
 die() { printf "FATAL: %s\n" "$2"; exit ;}
 
 # We have to use `env PATH=$PATH` so that used commands are ensured to use the correct PATH to see the expected binaries
 esudo() { sudo env "PATH=$PATH" "$@" ;}
 
-# Refer to https://github.com/srid/flake-root/discussions/5 for details tldr flake-root doesn't currently allow parsing the specific commit
-[ -n "$FLAKE_ROOT" ] || FLAKE_ROOT="github:kreyren/nixos-config/$(curl -s -X GET "https://api.github.com/repos/kreyren/nixos-config/commits?sha=add-tsvetan" | jq -r '.[0].sha')"
-
-[ -b "$systemDevice" ] || die 1 "Expected device was not found, refusing to install for safety"
+# [ -b "$systemDevice" ] || die 1 "Expected device was not found, refusing to install for safety"
 
 ragenixTempDir="/run/agenix"
 ragenixIdentity="$HOME/.ssh/id_ed25519"
@@ -23,15 +22,14 @@ ragenixIdentity="$HOME/.ssh/id_ed25519"
 # Manage Ragenix - The Rust implementation of age for managing secret files
 	# If the directory is not a symlink already then assume that the deployment is requested on a non-NiXium OS and mimic the file hierarchy
 	[ -L "$ragenixTempDir" ] || {
-		[ -d "$ragenixTempDir.d/1" ] || sudo mkdir --parents "$ragenixTempDir.d/1"
-		ln -s "$ragenixTempDir.d/1" "$ragenixTempDir"
+		[ -d "$ragenixTempDir.d/1" ] || sudo mkdir "$ragenixTempDir.d/1"
+		ln -s "$ragenixTempDir" "$ragenixTempDir.d/1"
 
 		# Ensure correct permission
-		sudo chown -R root:keys "$ragenixTempDir"
+		sudo chown -R "root:keys" "$ragenixTempDir"
 		sudo chmod -R 400 "$ragenixTempDir"
 	}
 
-	# The disk password has to be in /run/agenix/ for `disko-install` to not fail
 	[ -s "$ragenixTempDir/tsvetan-disks-password" ] || esudo age --identity "$ragenixIdentity" --decrypt --output "$ragenixTempDir/tsvetan-disks-password" "$secretTsvetanPasswordPath"
 
 	[ -s "$ragenixTempDir/tsvetan-ssh-ed25519-private" ] || esudo age --identity "$ragenixIdentity" --decrypt --output "$ragenixTempDir/tsvetan-ssh-ed25519-private" "$secretTsvetanKeyPath"
@@ -40,12 +38,12 @@ ragenixIdentity="$HOME/.ssh/id_ed25519"
 	swapFile="/swapfile"
 	# shellcheck disable=SC2312 # Upstream bug, should not output with the `|| echo 0` used as the de-factor '|| true' recommended
 	[ "$(awk '/SwapTotal/ {print $2}' /proc/meminfo || echo 0)" -ge 4194304 ] || {
-		! swapon -s | grep -q "$swapFile" || esudo swapoff "$swapFile" # Deactivate the swapfile if it's already there so that we can resize it
-		[ "$(stat -c%s "$swapFile" || echo 0)" -ge 4194304 ] || esudo dd if=/dev/zero of="$swapFile" bs=1M count=4096 conv=notrunc # Resize the swap file to the desired size
+		swapon -s | grep -q "$swapFile" || esudo swapoff "$swapFile" # Deactivate the swapfile if it's already there so that we can resize it
+		[ "$(stat -c%s "$swapFile" || echo 0)" -lt 4194304 ] || esudo dd if=/dev/zero of="$swapFile" bs=1M count=4096 conv=notrunc # Resize the swap file to the desired size
 
 		# Ensure correct permissions
 		esudo chmod 600 "$swapFile"
-		esudo chown root:root "$swapFile"
+		esudo chown root:root
 
 		esudo mkswap "$swapFile" # Make swap
 	}
@@ -56,15 +54,15 @@ ragenixIdentity="$HOME/.ssh/id_ed25519"
 		[ "$(stat -c%s "$swapFile" || echo 0)" -lt 4194304 ] || esudo swapon "$swapFile"
 	}
 
-nixos-rebuild build --flake "$FLAKE_ROOT#nixos-tsvetan-unstable" # pre-build the configuration
+nixos-rebuild build --flake "$FLAKE_ROOT#nixos-tsvetan-master" # pre-build the configuration
 
-# shellcheck disable=SC2312 # We are expecting to trigger a script failure if the `realpath` fails, apparently fixed in master shellcheck
-esudo disko-install \
-	--flake "$FLAKE_ROOT#nixos-tsvetan-unstable" \
-	--mode format \
+
+
+esudo nixos-anywhere \
+	--flake "$FLAKE_ROOT#nixos-tsvetan-master" \
+	-i "$ragenixIdentity" \
 	--debug \
-	--disk system "$(realpath "$systemDeviceBlock")" \
-	--extra-files "$ragenixTempDir/tsvetan-ssh-ed25519-private" /nix/persist/system/etc/ssh/ssh_host_ed25519_key
+	"$targetSystem"
 
 # FIXME(Krey): Flash u-boot, currently blocked by https://github.com/OLIMEX/DIY-LAPTOP/issues/73 (flashing it manually via SPI clamp and ch341a programmer atm)
 
