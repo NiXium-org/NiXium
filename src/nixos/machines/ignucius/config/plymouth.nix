152c20e1f84545592085aa9d3636727637885004
Jacob Hrbek <kreyren@fsfe.org>
2024-12-03 23:52:55 +0000

    adjust plymouth

--- src/nixos/machines/ignucius/config/plymouth.nix
+++ src/nixos/machines/ignucius/config/plymouth.nix
@@ -5,20 +5,6 @@
 let
 	inherit (lib) mkIf;
 in mkIf config.boot.plymouth.enable {
-	# BUG(Krey): Plymouth is currently broken and requires this fix: https://github.com/NixOS/nixpkgs/issues/332812
-	nixpkgs.overlays = [
-		(final: prev: {
-			plymouth = prev.plymouth.overrideAttrs ({ src, ... }: {
-				version = "24.004.60-unstable-2024-08-28";
-
-				src = src.override {
-					rev = "ea83580a6d66afd2b37877fc75248834fe530d99";
-					hash = "sha256-GQzf756Y26aCXPyZL9r+UW7uo+wu8IXNgMeJkgFGWnA=";
-				};
-			});
-		})
-	];
-
 	boot.plymouth = {
 		theme = "deus_ex";
 		themePackages = [
@@ -28,9 +14,7 @@ in mkIf config.boot.plymouth.enable {
 		];
 	};
 
-	#boot.initrd.availableKernelModules = ["i915"];
 	boot.kernelParams = [
-		"plymouth.use-simple-drm" # Workaround for a bug above
 		# Silent Boot
 		"quiet"
 		"splash"
