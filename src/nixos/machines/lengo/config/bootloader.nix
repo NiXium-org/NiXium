1be7db0dce0e7f904d47306da6a1581405966fb7
Jacob Hrbek <kreyren@fsfe.org>
2025-01-07 01:27:42 +0000

    sync

--- src/nixos/machines/lengo/config/bootloader.nix
+++ src/nixos/machines/lengo/config/bootloader.nix
@@ -3,10 +3,10 @@
 # Bootloader management of LENGO
 
 {
-	boot.lanzaboote.enable = true; # Whether to use NixOS's implementation of secure-boot
+	boot.lanzaboote.enable = false; # Whether to use NixOS's implementation of secure-boot
 
 	# Lanzaboote imports it's own systemd bootloader
-	boot.loader.systemd-boot.enable = false; # Use SystemD boot loader
+	boot.loader.systemd-boot.enable = true; # Use SystemD boot loader
 
 	boot.loader.efi.canTouchEfiVariables = true;
 }
